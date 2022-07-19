eccc61901ea093e210c10590fb834315a64845b1
Muzamil Ahmad <66633369+Muzamil-07@users.noreply.github.com>
2022-07-19 21:21:35 +0500

    small changes

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import {
 } from "react-router-dom";
 import Admin from './components/Admin/Admin';
 import Dashboard from './components/Dashboard';
+import ProtectedRoute from './components/ProtectedRoute';
 
 
 function App() {
@@ -17,10 +18,17 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+
+          <Route exact path="admin" element={
+            <ProtectedRoute role={[ 'admin' ]}>  <Admin /> </ProtectedRoute>
+          } />
+
+          <Route exact path="dashboard" element={
+            <ProtectedRoute role={[ 'user' ]}>  <Dashboard /> </ProtectedRoute>
+          } />
+
           <Route path='login' element={<Login />} />
           <Route path='/' element={<Signup />} />
-          <Route path='admin' element={<Admin />} />
-          <Route path='dashboard' element={<Dashboard />} />
 
         </Routes>
       </BrowserRouter>
