8d1d002fa021fe408d9f2d1c2bfcf78a601dee7b
unknown <rfasih14@gmail.com>
2022-07-20 19:12:35 +0500

    logout

--- client/src/components/Admin/Admin.js
+++ client/src/components/Admin/Admin.js
@@ -5,16 +5,32 @@ import { Breadcrumb } from 'antd';
 import './Admin.css'
 import PersonIcon from '@mui/icons-material/Person';
 import LogoutIcon from '@mui/icons-material/Logout';
-
+import Cookies from 'js-cookie';
+import { useNavigate  } from 'react-router-dom';
 import UsersTable from './UsersTable';
 
 export default function Admin() {
-  return (
+  const navigate = useNavigate();
+  
+  const handleLogout=() => {
+    Cookies.remove( 'jwt' );
+
+  }
+
+const handleClick = ()=>{
+
+  setTimeout( () => {
+    handleLogout();
+    navigate( '/login' );
+  
+  }, 1000 )
+}
+return (
     <>
 
     <div className='admin_nav'>
     <span>Baccarat</span>
-    <span> <a><PersonIcon fontSize='20px' sx={{marginRight:'5px'}}/>Profile</a> <a style={{paddingLeft:'2rem'}}><LogoutIcon fontSize='20px' sx={{marginRight:'5px'}}/>Logout</a> </span>
+    <span> <a><PersonIcon fontSize='20px' sx={{marginRight:'5px'}}/>Profile</a> <a onClick={handleClick} style={{paddingLeft:'2rem'}}><LogoutIcon fontSize='20px' sx={{marginRight:'5px'}}/>Logout</a> </span>
     </div>
     <div style={{marginTop:'5rem' , marginLeft:'1rem' , marginBottom:'-1.5rem'}}>
 
